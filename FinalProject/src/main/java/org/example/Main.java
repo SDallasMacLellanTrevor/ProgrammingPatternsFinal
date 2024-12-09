8e86f5b3a0aed4bae37ad0745a97b46ad6288309
SDallasMacLellanTrevor <maclellan.trevor@gmail.com>
2024-12-08 22:10:50 -0500

    Created final draft
    Removed commented out methods in main to clean up code

--- FinalProject/src/main/java/org/example/Main.java
+++ FinalProject/src/main/java/org/example/Main.java
@@ -1,49 +1,9 @@
 package org.example;
 
 import org.example.control.WindowController;
-import org.example.view.MainWindowView;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-//        String userUrl = "jdbc:sqlite:./FinalProject/src/main/resources/userAccounts.db";
-//        String adminUrl =  "jdbc:sqlite:./FinalProject/src/main/resources/adminAccounts.db";
-//        String bankUrl =  "jdbc:sqlite:./FinalProject/src/main/resources/bankAccounts.db";
-//
-//        try (Connection connection = DriverManager.getConnection(userUrl);
-//             Statement statement = connection.createStatement()) {
-//            String SQL = """
-//                DROP TABLE IF EXISTS userAccounts
-//                """;
-//            statement.executeUpdate(SQL);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        try (Connection connection = DriverManager.getConnection(adminUrl);
-//             Statement statement = connection.createStatement()) {
-//            String SQL = """
-//                DROP TABLE IF EXISTS  adminAccounts
-//                """;
-//            statement.executeUpdate(SQL);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        try (Connection connection = DriverManager.getConnection(bankUrl);
-//             Statement statement = connection.createStatement()) {
-//            String SQL = """
-//                DROP TABLE IF EXISTS  bankAccounts
-//                """;
-//            statement.executeUpdate(SQL);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-
         WindowController.initialize();
     }
 }
